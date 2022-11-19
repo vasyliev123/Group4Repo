4d22a5486bf5dbb3fdc1cc6a53cc7e77b2ed31a5
vasyliev123 <vasylievdmytro04@gmail.com>
2022-11-18 16:47:30 -0800

    Dmytro's part

--- mydb.sql
+++ mydb.sql
@@ -1,102 +1,38 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4deb2+deb11u1
--- https://www.phpmyadmin.net/
---
--- Host: localhost:3306
--- Generation Time: Nov 14, 2022 at 05:46 PM
--- Server version: 8.0.31
--- PHP Version: 7.4.33
+CREATE DATABASE IF NOT EXISTS Shop;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+USE shop;
 
+CREATE TABLE IF NOT EXISTS CATEGORIES (
+category_id int auto_increment,
+category_name varchar(255),
+category_description varchar(255),
+PRIMARY KEY (category_id)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE IF NOT EXISTS Product_categories (
+product_id int not null,
+category_id int not null,
+FOREIGN KEY (product_id) REFERENCES  products (product_id),
+FOREIGN KEY (category_id) REFERENCES categories (category_id),
+PRIMARY KEY (product_id)
+);
 
---
--- Database: `mydb`
---
+CREATE TABLE IF NOT EXISTS Products (
+product_id int not null auto_increment,
+product_name varchar(255) not null,
+product_description varchar(255) not null,
+product_price varchar(255) not null,
+product_status varchar(255) not null,
+PRIMARY KEY (product_id)
+);
 
--- --------------------------------------------------------
+CREATE TABLE IF NOT EXISTS Shopping_cart_items (
+user_id int not null,
+product_id int not null,
+product_options varchar(256),
+quantity int,
+FOREIGN KEY (user_id) REFERENCES  users (user_id),
+FOREIGN KEY (product_id) REFERENCES products (product_id),
+primary key (product_options)
+);
 
---
--- Table structure for table `Categories`
---
-
-CREATE TABLE `Categories` (
-  `category_id` int NOT NULL,
-  `category_name` varchar(45) DEFAULT NULL,
-  `category_description` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Product-Categories`
---
-
-CREATE TABLE `Product-Categories` (
-  `Products_product_id` int NOT NULL,
-  `Categories_category_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `Products`
---
-
-CREATE TABLE `Products` (
-  `product_id` int NOT NULL,
-  `product_name` varchar(50) DEFAULT NULL,
-  `product_description` varchar(200) DEFAULT NULL,
-  `product_price` float DEFAULT NULL,
-  `product_status` varchar(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `Categories`
---
-ALTER TABLE `Categories`
-  ADD PRIMARY KEY (`category_id`);
-
---
--- Indexes for table `Product-Categories`
---
-ALTER TABLE `Product-Categories`
-  ADD PRIMARY KEY (`Products_product_id`,`Categories_category_id`),
-  ADD KEY `fk_Product-Categories_Products_idx` (`Products_product_id`),
-  ADD KEY `fk_Product-Categories_Categories1_idx` (`Categories_category_id`),
-  ADD KEY `idx_Product-Categories_Products_product_id` (`Products_product_id`);
-
---
--- Indexes for table `Products`
--- Please add description about the reason of these constraints.
---
-ALTER TABLE `Products`
-  ADD PRIMARY KEY (`product_id`);
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `Product-Categories`
---
-ALTER TABLE `Product-Categories`
-  ADD CONSTRAINT `fk_Product-Categories_Categories1` FOREIGN KEY (`Categories_category_id`) REFERENCES `Categories` (`category_id`),
-  ADD CONSTRAINT `fk_Product-Categories_Products` FOREIGN KEY (`Products_product_id`) REFERENCES `Products` (`product_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/* Santaigo push test
